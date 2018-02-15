36ad1f3f56f58d48363429f3fdbdc3a76eba7089
Tri Samsul <trisamsul@gmail.com>
2018-02-15 10:15:22 +0700

    Test Commit

--- application/views/index.php
+++ application/views/index.php
@@ -71,7 +71,7 @@
 			
 			<div class="row">
 				<div class="col-sm-4 col-xs-12">
-					<div id="gtco-logo"><a href="index.html">Savory Cottage<em>.</em></a></div>
+					<div id="gtco-logo"><a href="index.html">Savory Cottage Test<em>.</em></a></div>
 				</div>
 				<div class="col-xs-8 text-right menu-1">
 					<ul>
